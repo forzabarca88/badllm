4a30eb3d5c67bce652e5a92434442d565b860367
forzabarca88 <you@example.com>
2025-08-27 18:11:03 +1000

    simple chatbot implementation with limited memory

--- main.py
+++ main.py
@@ -1,10 +1,37 @@
-from ollama import chat
+import asyncio
+from pprint import pprint
+import logging
+from ollama import AsyncClient
 
-stream = chat(
-    model='phi3.5:3.8b',
-    messages=[{'role': 'user', 'content': 'Why is the sky blue?'}],
-    stream=True,
-)
+logging.basicConfig(level=logging.INFO)
 
-for chunk in stream:
-  print(chunk['message']['content'], end='', flush=True)
+MODEL='llama3.2'
+SYSTEM='You are a comedian with a deadpan style - answer all questions factually but with humor'
+
+
+async def chat(model=MODEL, system_message=SYSTEM):
+    all_messages = []
+    while True:
+        user_message = input("\nEnter your message: ")
+        all_messages = generate_request(all_messages, user_message, system_message)
+        logging.info(pprint(all_messages))
+        full_response = []
+        async for part in await AsyncClient().chat(model=model, messages=all_messages, stream=True):
+            print(part['message']['content'], end='', flush=True)
+            full_response.append(part['message']['content'])
+        all_messages.append({'role': 'assistant', 'content': ''.join(full_response)})
+
+
+def generate_request(all_messages, user_message, system_message=SYSTEM):
+    all_messages = all_messages[-7:]
+    all_messages.extend(
+        [
+            {'role': 'system', 'content': system_message},
+            {'role': 'user', 'content': user_message}
+        ]
+    )
+    return all_messages
+
+
+if __name__ == '__main__':
+    asyncio.run(chat())
