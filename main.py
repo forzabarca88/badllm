ddcb3267c30dac40ea41cc901927e2015c7b4184
forzabarca88 <you@example.com>
2025-08-27 17:00:14 +1000

    initial

---
+++ main.py
@@ -0,0 +1,10 @@
+from ollama import chat
+
+stream = chat(
+    model='phi3.5:3.8b',
+    messages=[{'role': 'user', 'content': 'Why is the sky blue?'}],
+    stream=True,
+)
+
+for chunk in stream:
+  print(chunk['message']['content'], end='', flush=True)
